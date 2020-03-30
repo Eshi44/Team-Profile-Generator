6d4d2b7604f56ce11d653cd6f24bd1a2d9565f03
Eshi44 <e.hameliervolino@gmail.com>
2020-03-30 17:15:54 -0400

    added engineer class to engineer.js file

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,33 +1,32 @@
 // The first class is an `Employee` parent class with the following properties and
 // methods:
 class Employee {
-    constructor(name,id,email) {
-        //   * name
-        this.name = name;
-        //   * id
-        this.id = id;
-        //   * email
-        this.email = email;
-    }
-    
-    // methods:
-    //   * getName()
-    getName() {
-        return this.name;
-    }
-    //   * getId()
-    getID() {
-        return this.id;
-    }
-    //   * getEmail()
-    getEmail() {
-        return this.email;
-    }
-    //   * getRole() // Returns 'Employee'
-    getRole() {
-        return 'Employee';
-    }
-}
+  constructor(name, id, email) {
+    //   * name
+    this.name = name;
+    //   * id
+    this.id = id;
+    //   * email
+    this.email = email;
+  }
 
+  // methods:
+  //   * getName()
+  getName() {
+    return this.name;
+  }
+  //   * getId()
+  getID() {
+    return this.id;
+  }
+  //   * getEmail()
+  getEmail() {
+    return this.email;
+  }
+  //   * getRole() // Returns 'Employee'
+  getRole() {
+    return "Employee";
+  }
+}
 
 module.exports = Employee;
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,22 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// In addition to `Employee`'s properties and methods, `Engineer` will also have:
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    //   * github  // GitHub username
+    this.github = github;
+  }
+
+  //   * getGithub()
+  getGithub() {
+    return this.github;
+  }
+
+  //   * getRole() // Overridden to return 'Engineer'
+  getRole() {
+    return "Engineer";
+  }
+}
+
+module.exports = Engineer;
