aba08a16f465cc56e199f379aebaae8d4c85b7f8
Eshi44 <e.hameliervolino@gmail.com>
2020-03-30 17:22:31 -0400

    added intern class, properties and methods to intern.js file

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// In addition to `Employee`'s properties and methods, `Intern` will also have:
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    //   * school
+    this.school = school;
+  }
+  //   * getSchool()
+  getSchool() {
+    return this.school;
+  }
+  //   * getRole() // Overridden to return 'Intern'
+  getRole() {
+    return "Intern";
+  }
+}
+
+module.exports = Intern;
