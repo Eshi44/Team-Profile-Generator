abbe2999218ca0ec01decac12d55b682e8c76792
Eshi44 <e.hameliervolino@gmail.com>
2020-03-30 17:27:12 -0400

    added manager class, properties, and method to manager.js

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,15 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// In addition to `Employee`'s properties and methods, `Manager` will also have:
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    //   * officeNumber
+    this.officeNumber = officeNumber;
+  }
+
+  //   * getRole() // Overridden to return 'Manager'
+  getRole() {
+    return "Manager";
+  }
+}
+
+module.exports = Manager;
