d5276ef53d13e0b551bc5d5f12f03c6a0a6c6e89
Eshi44 <e.hameliervolino@gmail.com>
2020-03-31 16:25:46 -0400

    made some final adjustments

--- Develop/apps.js
+++ Develop/apps.js
@@ -6,7 +6,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const promisify = require("./node_modules/util.promisify");
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
